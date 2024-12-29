0842e2143ee8e1abf8cd288e4a481b327181120a
Rusheeta Imandi <rimandi2@gitam.in>
2024-12-29 11:12:08 +0530

    Corrected spelling mistakes

--- String reversal.py
+++ String reversal.py
@@ -1,4 +1,4 @@
-#String reversal: Given an string with words separated by empty spaces, commas or fullstops, write a program to return the string after removing the commas and fullstops and reversing the words in the string
+#String reversal: Given an string with words separated by empty spaces, commas or fullstops, write a program to return the string after removing the commas and fullstops and reversing the words in the string.
 
 def reverse_words(input_string):
     cleaned_string = input_string.replace(',', '').replace('.', '')
