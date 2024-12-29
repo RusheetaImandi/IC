68bfc7c472e6a7ec0c3deaa5249c0825325e8948
RusheetaImandi <rimandi2@gitam.in>
2024-12-29 11:39:39 +0530

    Is substring.py

--- Is substring.py
+++ Is substring.py
@@ -1,13 +1,9 @@
 #Is substring: Given two strings str1 and str2, write a program to return true if str2 is a substring of str1 and false otherwise.
 
 def is_substring(str1, str2):
-    # Check if str2 is in str1
     return str2 in str1
-
-# Take input from the user
+    
 str1 = input("Enter the first string (str1): ")
 str2 = input("Enter the second string (str2): ")
-
-# Check if str2 is a substring of str1
 result = is_substring(str1, str2)
 print(f"Is '{str2}' a substring of '{str1}'? {result}")
